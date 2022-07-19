55076d7ab51efa7abf9cd3fb9d087b476e89bf65
Haritiana Adriano <103512767+haritianaadriano@users.noreply.github.com>
2022-07-19 05:18:17 +0300

    Update paiementClient.sql

--- queries/paiementClient.sql
+++ queries/paiementClient.sql
@@ -1,20 +1,19 @@
---Cette requette affiche tout les details d'un client sur son paiement
---Documentation : payement_complet: référencie au oui ou non le client a-t-il payé en entier
-SELECT date_payement, payement_complet, montant_paye, id_client 
+/* Cette requette affiche tout les details d'un client sur son paiement */
+/* Documentation : payement_complet: référencie au oui ou non le client a-t-il payé en entier 
+   Documentation sur le resultat: le resultat suivantaffiche f et t pour payement complet: t=true et f=false
+*/
+SELECT date_payement, payement_complet, montant_paye, nom, prenom, telephone 
         FROM payement 
         INNER JOIN reservation 
-        ON reservation.id_reservation = payement.id_reservation; 
+        ON payement.id_reservation = reservation.id_reservation
+        INNER JOIN client
+        ON reservation.id_client = client.id_client; 
 
 /*
-date_payement | payement_complet | montant_paye | id_client 
----------------+------------------+--------------+-----------
- 2022-03-21    | f                |       144043 |        33
- 2022-01-24    | t                |       124419 |        42
- 2022-02-03    | f                |       116548 |        49
- 2021-08-13    | t                |       161215 |        50
- 2021-09-19    | f                |       112073 |        25
- 2022-02-18    | f                |       115980 |         5
- 2022-03-14    | t                |        90153 |        25
- 2022-07-08    | t                |        96611 |        36
- 2022-03-23    | t                |       166541 |         9
-*/
+         date_payement | payement_complet | montant_paye |   nom    |  prenom   |  telephone
+---------------+------------------+--------------+----------+-----------+--------------
+ 2022-03-21    | f                |       144043 | Vivyan   | MacGarvey | 636-883-9627
+ 2021-08-13    | t                |       161215 | Lilas    | Merryman  | 738-693-2581
+ 2021-09-19    | f                |       112073 | Gussy    | Parratt   | 623-801-9402
+ 2022-07-05    | f                |        89702 | Gayelord | Linnell   | 389-788-1995
+ */
